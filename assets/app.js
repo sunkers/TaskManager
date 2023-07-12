96679180f47c01abd8afbca3499dd65b55456c47
lmougin <mougin.loic@gmail.com>
2023-07-12 16:16:58 +0200

    intégration TailWindCSS

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,4 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+console.log('Hello Webpack Encore! Edit me in assets/app.js');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
